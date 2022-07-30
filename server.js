4181300a554dee6744a207110c617e29394fa527
mattgaither <benjamin.gaither@shipt.com>
2022-07-30 08:08:33 -0500

    added default connection from module activity

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set('debug', true);
