434e4df10af59236125068f2164bff1c6e07fe96
mattgaither <benjamin.gaither@shipt.com>
2022-08-19 19:13:38 -0500

    completed the reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -29,3 +29,33 @@ const ThoughtSchema = new Schema(
     id : false,
   }
 );
+
+const ReactionSchema = new Schema(
+  {
+    reactionID: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtValue) => dateFormat(createdAtValue),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  },
+);
+
